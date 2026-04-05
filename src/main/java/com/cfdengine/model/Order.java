5b04140143de8f17659c5904018743d92d27fce7
Pavel-OOP <pavel.i.petrov.7@gmail.com>
2026-04-05 12:13:01 +0300

    Finished order class

--- src/main/java/com/cfdengine/model/Order.java
+++ src/main/java/com/cfdengine/model/Order.java
@@ -1,17 +1,32 @@
 package com.cfdengine.model;
 
 import com.cfdengine.model.enums.Direction;
+import com.cfdengine.model.enums.OrderStatus;
 
 import java.math.BigDecimal;
+import java.time.Instant;
 
 public abstract class Order {
     private final Direction direction;
     private final String instrument;
     private final BigDecimal size;
+    private final Instant createdAt;
+    private OrderStatus status;
+
 
     protected Order(Direction direction, String instrument, BigDecimal size) {
         this.direction = direction;
         this.instrument = instrument;
         this.size = size;
+        this.status = OrderStatus.PENDING;
+        this.createdAt = Instant.now();
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
     }
 }
